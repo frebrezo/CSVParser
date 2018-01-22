1f3a1eb895a10db3c4fd8b5c9be8715f60504c88
kimwon <kimwon@iit.edu>
2018-01-21 19:12:28 -0500

    Initial.

---
+++ CSVParser.UnitTest/CsvParserTest.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CSVParser.UnitTest
+{
+	/// <summary>
+	/// Unit tests for CsvParser.
+	/// </summary>
+	[TestClass]
+	public class CsvParserTest
+	{
+		[TestMethod]
+		public void ParseCsvLine_HappyPathDoubleQuotedTest()
+		{
+			// "field1","field2","field3"
+			string csvLine = @"""field1"",""field2"",""field3""";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(3, fieldList.Count);
+			Assert.AreEqual("field1", fieldList[0]);
+			Assert.AreEqual("field2", fieldList[1]);
+			Assert.AreEqual("field3", fieldList[2]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_HappyPathDoubleQuotedWithSpacesTest()
+		{
+			// " field1  ","  field2  ","field3   "
+			string csvLine = @""" field1  "",""  field2  "",""field3   """;
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(3, fieldList.Count);
+			Assert.AreEqual(" field1  ", fieldList[0]);
+			Assert.AreEqual("  field2  ", fieldList[1]);
+			Assert.AreEqual("field3   ", fieldList[2]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_HappyPathTest()
+		{
+			// field1,field2,field3
+			string csvLine = @"field1,field2,field3";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(3, fieldList.Count);
+			Assert.AreEqual("field1", fieldList[0]);
+			Assert.AreEqual("field2", fieldList[1]);
+			Assert.AreEqual("field3", fieldList[2]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_HappyPathExtraSpacesTest()
+		{
+			//  field1 , field2 , field3   
+			string csvLine = @" field1 , field2 , field3   ";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(3, fieldList.Count);
+			Assert.AreEqual("field1", fieldList[0]);
+			Assert.AreEqual("field2", fieldList[1]);
+			Assert.AreEqual("field3", fieldList[2]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_EscapedDoubleQuotesTest()
+		{
+			// "field1 with ""doublequote""","field2","field3"
+			string csvLine = @"""field1 with """"doublequote"""""",""field2"",""field3""";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(3, fieldList.Count);
+			Assert.AreEqual("field1 with \"doublequote\"", fieldList[0]);
+			Assert.AreEqual("field2", fieldList[1]);
+			Assert.AreEqual("field3", fieldList[2]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_EscapedDoubleAndCommaTest()
+		{
+			// "field1 with ""doublequote""","field2.1,field2.2","field3"
+			string csvLine = @"""field1 with """"doublequote"""""",""field2.1,field2.2"",""field3""";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(3, fieldList.Count);
+			Assert.AreEqual("field1 with \"doublequote\"", fieldList[0]);
+			Assert.AreEqual("field2.1,field2.2", fieldList[1]);
+			Assert.AreEqual("field3", fieldList[2]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_MissingDoubleQuoteAtEnd()
+		{
+			// "field1","field2","field3"
+			string csvLine = @"""field1"",""field2"",""field3";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(3, fieldList.Count);
+			Assert.AreEqual("field1", fieldList[0]);
+			Assert.AreEqual("field2", fieldList[1]);
+			Assert.AreEqual("field3", fieldList[2]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_MissingDoubleQuoteInMiddle()
+		{
+			// "field1",field2","field3"
+			string csvLine = @"""field1"",field2"",""field3""";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(3, fieldList.Count);
+			Assert.AreEqual("field1", fieldList[0]);
+			Assert.AreEqual("field2\"", fieldList[1]);
+			Assert.AreEqual("field3", fieldList[2]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_ExtraDoubleQuoteInMiddle()
+		{
+			// "field1",field2"","field3"
+			string csvLine = @"""field1"",field2"""",""field3""";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(3, fieldList.Count);
+			Assert.AreEqual("field1", fieldList[0]);
+			Assert.AreEqual("field2\"\"", fieldList[1]);
+			Assert.AreEqual("field3", fieldList[2]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_EmptyFieldDoubleQuoted()
+		{
+			// "","field1","field2","field3"
+			string csvLine = @""""",""field1"",""field2"",""field3""";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(4, fieldList.Count);
+			Assert.AreEqual("", fieldList[0]);
+			Assert.AreEqual("field1", fieldList[1]);
+			Assert.AreEqual("field2", fieldList[2]);
+			Assert.AreEqual("field3", fieldList[3]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_EmptyField()
+		{
+			// ,field1,field2,field3
+			string csvLine = @",field1,field2,field3";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(4, fieldList.Count);
+			Assert.AreEqual("", fieldList[0]);
+			Assert.AreEqual("field1", fieldList[1]);
+			Assert.AreEqual("field2", fieldList[2]);
+			Assert.AreEqual("field3", fieldList[3]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_UnescapedDoubleQuote()
+		{
+			// field1,"field2",field3 "random doublequote", field4 with space
+			string csvLine = @"field1,""field2"",field3 ""random doublequote"", field4 with space";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(4, fieldList.Count);
+			Assert.AreEqual("field1", fieldList[0]);
+			Assert.AreEqual("field2", fieldList[1]);
+			Assert.AreEqual("field3 \"random doublequote\"", fieldList[2]);
+			Assert.AreEqual("field4 with space", fieldList[3]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_UnescapedDoubleQuoteInDoubleQuotedField()
+		{
+			// "field1","field2 "","field3"
+			string csvLine = @"""field1"",""field2 """",""field3""";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(2, fieldList.Count);
+			Assert.AreEqual("field1", fieldList[0]);
+			Assert.AreEqual("field2 \",field3\"", fieldList[1]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_UnescapedDoubleQuoteInDoubleQuotedFieldInMiddle()
+		{
+			// "field1","field2 "","field3",field 4
+			string csvLine = @"""field1"",""field2 """",""field3"",field 4";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(3, fieldList.Count);
+			Assert.AreEqual("field1", fieldList[0]);
+			Assert.AreEqual("field2 \",field3\"", fieldList[1]);
+			Assert.AreEqual("field 4", fieldList[2]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_UnescapedDoubleQuoteInDoubleQuotedFieldInMiddleAndTripleDoubleQuoteAtEnd()
+		{
+			// "field1","field2 "","field3",field """4
+			string csvLine = @"""field1"",""field2 """",""field3"",field """"""4";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(3, fieldList.Count);
+			Assert.AreEqual("field1", fieldList[0]);
+			Assert.AreEqual("field2 \",field3\"", fieldList[1]);
+			Assert.AreEqual("field \"\"\"4", fieldList[2]);
+		}
+
+		[TestMethod]
+		public void ParseCsvLine_UnescapedDoubleQuoteInDoubleQuotedFieldInBeginningAndMiddleAndTripleDoubleQuoteAtEnd()
+		{
+			// ""field1","field2 "","field3",field """4
+			string csvLine = @"""""field1"",""field2 """",""field3"",field """"""4";
+			CsvParser csvParser = new CsvParser();
+			IList<string> fieldList = csvParser.ParseCsvLine(csvLine, ',');
+			Assert.AreEqual(3, fieldList.Count);
+			Assert.AreEqual("field1\"", fieldList[0]);
+			Assert.AreEqual("field2 \",field3\"", fieldList[1]);
+			Assert.AreEqual("field \"\"\"4", fieldList[2]);
+		}
+	}
+}
---
+++ CSVParser/CsvParser.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVParser
+{
+	/// <summary>
+	/// CSV parser. CsvParser follows Microsoft Excel parsing conventions.
+	/// </summary>
+	public class CsvParser
+	{
+		/// <summary>
+		/// Parse line into CSV record array
+		/// </summary>
+		/// <param name="line"></param>
+		/// <param name="delimiter"></param>
+		/// <returns></returns>
+		public IList<string> ParseCsvLine(string line, char delimiter)
+		{
+			StringBuilder fieldVal = new StringBuilder();
+			IList<string> lineArr = new List<string>();
+			bool startsWithDQuote = false;
+			bool unexpectedDQuote = false;
+			int cInt = 0;
+			char c = (char)0;
+			int quotedCInt = 0;
+			char quotedC = (char)0;
+			int nextQuotedCInt = 0;
+			char nextQuotedC = (char)0;
+			using (System.IO.StringReader strBuffer = new System.IO.StringReader(line))
+			{
+				// Loop over each character in string line.
+				while ((cInt = strBuffer.Read()) > 0)
+				{
+					c = (char)cInt;
+					// If the field value starts with a doublequote (") then set startsWithDQuote = true.
+					if (fieldVal.Length == 0 && c == '"')
+					{
+						startsWithDQuote = true;
+					}
+					// If a delimiter has been reached, add the field value to lineArr.
+					//		Otherwise, continue looking for the end of the field value.
+					if (c == delimiter)
+					{
+						lineArr.Add(startsWithDQuote ? fieldVal.ToString() : fieldVal.ToString().Trim());
+						fieldVal.Length = 0;
+						startsWithDQuote = false;
+						unexpectedDQuote = false;
+					}
+					else
+					{
+						// If c == doublequote ("), scan for a matching doublequote (") character.
+						if (c == '"')
+						{
+							// If the field value length > 0 and the field doesn't start with a
+							//		doublequote (") then a doublequote (") was encountered in the
+							//		middle of the field value. Append it to field value.
+							if (fieldVal.Length > 0 && !startsWithDQuote)
+							{
+								fieldVal.Append(c);
+							}
+							// Scan for end of the field.
+							while ((quotedCInt = strBuffer.Read()) > 0)
+							{
+								quotedC = (char)quotedCInt;
+								// If field value does NOT start with a doublequote (") and the
+								//		delimiter character has been encoutered, then the
+								//		end of the field value has been found.
+								//		Add the field to lineArr and break out of the loop.
+								//		Otherwise, a delimiter was found in the middle of a
+								//		field value surrounded with doublequotes ("), so add the
+								//		delimiter to the field value.
+								//		If doublequote (") character is encoutered in the middle
+								//		of the field value and the field value starts with
+								//		a doublequote (") character then doublequote (") must be
+								//		escaped.
+								if (!startsWithDQuote && quotedC == delimiter)
+								{
+									lineArr.Add(startsWithDQuote ? fieldVal.ToString() : fieldVal.ToString().Trim());
+									fieldVal.Length = 0;
+									startsWithDQuote = false;
+									unexpectedDQuote = false;
+									break;
+								}
+								else if (quotedC == '"')
+								{
+									if (fieldVal.Length > 0 && !startsWithDQuote)
+									{
+										fieldVal.Append(quotedC);
+									}
+									// Read the immediate next character and add it to
+									//		the field value. If the immediate next character
+									//		is a delimiter then add field value to lineArr and
+									//		break out of the loop.
+									if ((nextQuotedCInt = strBuffer.Read()) > 0)
+									{
+										nextQuotedC = (char)nextQuotedCInt;
+										// The field value starts with a doublequote (") character,
+										//		but a doublequote (") was encountered without
+										//		a matching doublequote ("). If a doublequote (")
+										//		is encountered at the end of a field value,
+										//		then add the final doublequote (") character.
+										if (startsWithDQuote && quotedC == '"' && nextQuotedC != '"' && nextQuotedC != delimiter)
+										{
+											unexpectedDQuote = true;
+										}
+										if (nextQuotedC == delimiter)
+										{
+											if (unexpectedDQuote && quotedC == '"')
+											{
+												fieldVal.Append(quotedC);
+											}
+											lineArr.Add(startsWithDQuote ? fieldVal.ToString() : fieldVal.ToString().Trim());
+											fieldVal.Length = 0;
+											startsWithDQuote = false;
+											unexpectedDQuote = false;
+											break;
+										}
+										else
+										{
+											fieldVal.Append(nextQuotedC);
+										}
+									}
+									else if (unexpectedDQuote && quotedC == '"')
+									{
+										// If a final doublequote (") is encountered,
+										//		then add the final doublequote (") character.
+										fieldVal.Append(quotedC);
+									}
+								}
+								else
+								{
+									fieldVal.Append(quotedC);
+								}
+							}
+						}
+						else
+						{
+							fieldVal.Append(c);
+						}
+					}
+				}
+				// After looping all the characters in string line, if field value contains
+				//		a value, add it to lineArr.
+				if (fieldVal.Length > 0)
+				{
+					lineArr.Add(startsWithDQuote ? fieldVal.ToString() : fieldVal.ToString().Trim());
+					fieldVal.Length = 0;
+					startsWithDQuote = false;
+					unexpectedDQuote = false;
+				}
+			}
+			return lineArr;
+		}
+	}
+}
